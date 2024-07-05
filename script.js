17d7b3a87e39d5af39670f0095a495d7ceddc816
Sushrutha Shanbhogue <137515081+Sushruthashan@users.noreply.github.com>
2024-07-05 19:15:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+function Validation() {
+    
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
+    var emailpattern = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]$/";
+    var passwordpattern = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).";
+    var usernamepattern = "^[A-Za-z][A-Za-z0-9_]$";
+
+    if(username === '' || email === '' || password === '')
+    {
+        console.log("All fields must be filled");
+        return false;
+    }
+    
+    if(!usernamepattern.test(username))
+    {
+        return false;
+    }
+
+    if(!emailpattern.test(email))
+    {
+        return false;
+    }
+
+    if(!passwordpattern.test(password))
+    {
+        return false;
+    }
+
+    return true;
+
+    console.log("Logged in successfully");
+}
